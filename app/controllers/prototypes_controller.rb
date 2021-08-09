4798ec8c57d5aee19b003794cea92296b91a7cb4
keita12 <18b1228@andrew.ac.jp>
2021-08-09 11:30:22 +0900

    issueの修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -24,6 +24,9 @@ class PrototypesController < ApplicationController
   end
   def edit
     @prototype = Prototype.find(params[:id])
+    if current_user.id != @prototype.user_id
+      redirect_to root_path
+    end
   end
   def update
     @prototype = Prototype.find(params[:id])
